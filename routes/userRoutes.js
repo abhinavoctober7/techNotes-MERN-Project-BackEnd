4fb09888f52b7d98968b93b800998490bbf222db
abhinav <abhinavoctober7@gmail.com>
2023-04-23 21:00:07 +0530

    fourth commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = require("express").Router();
+const {
+  getAllUsers,
+  createNewUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/usersController");
+
+router
+  .route("/")
+  .get(getAllUsers)
+  .post(createNewUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -22,7 +22,9 @@ app.use(cookieParser());
 app.use(cors(corsOptions));
 
 app.use("/", express.static(path.join(__dirname, "public")));
+
 app.use("/", require("./routes/root"));
+app.use("/users", require("./routes/userRoutes"));
 
 app.all("*", (req, res) => {
   res.status(404);
